244aac150a7398fc9a6490929b3f6f0683adb62c
AnkurYadav65439 <ay0695617@gmail.com>
2024-06-28 22:21:45 +0530

    courses page

---
+++ app/(main)/courses/list.tsx
@@ -0,0 +1,50 @@
+'use client'
+import { courses, userProgress } from "@/db/schema"
+import Card from "./card";
+import { useTransition } from "react";
+import { useRouter } from "next/navigation";
+import { upsertUserProgress } from "@/actions/user-progress";
+import { toast } from "sonner"
+
+type Props = {
+    courses: typeof courses.$inferSelect[];
+    activeCourseId?: typeof userProgress.$inferSelect.activeCourseId;
+}
+
+const List = ({ courses, activeCourseId }: Props) => {
+    const [pending, startTransition] = useTransition();
+    const router = useRouter();
+
+    const onClick = (id: number) => {
+        if (pending) return;
+
+        console.log(" list : ", id, activeCourseId)
+        if (id === activeCourseId) {
+            console.log('if router')
+            router.push("/learn");
+        } else {
+            startTransition(() => {
+                upsertUserProgress(id)
+                    .catch((err) => toast.error(err.toString()));
+            })
+        }
+    }
+
+    return (
+        <div className="pt-6 grid grid-cols-2 lg:grid-cols-[repeat(auto-fill,minmax(210px,1fr))] gap-4">
+            {courses.map((course) => (
+                <Card
+                    key={course.id}
+                    id={course.id}
+                    title={course.title}
+                    imageSrc={course.imageSrc}
+                    onClick={onClick}
+                    disabled={pending}
+                    active={course.id === activeCourseId}
+                />
+            ))}
+        </div>
+    )
+}
+
+export default List
--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -3,20 +3,30 @@ import StickyWrapper from '@/components/shared/sticky-wrapper'
 import React from 'react'
 import Header from './header'
 import UserProgress from '@/components/shared/user-progress'
+import { getUserProgress } from '@/db/queries'
+import { redirect } from 'next/navigation'
 
-const LearnPage = () => {
+const LearnPage = async () => {
+    const userProgressData = getUserProgress();
+
+    const [userProgress] = await Promise.all([userProgressData]);
+
+    if (!userProgress || !userProgress.activeCourse) {
+        redirect("/courses");
+    }
+
     return (
         <div className='flex flex-row-reverse gap-[48px] px-6 '>
             <StickyWrapper>
                 <UserProgress
-                    activeCourse={{ title: "Spanish", imageSrc: "/es.svg" }}
+                    activeCourse={{ title: userProgress.activeCourse.title, imageSrc: userProgress.activeCourse.imageSrc }}
                     hearts={5}
                     points={100}
                     hasActiveSubscription={false}
                 />
             </StickyWrapper>
             <FeedWrapper>
-                <Header title="Spanish" />
+                <Header title={userProgress.activeCourse.title} />
                 <div className='space-y-4'>
                     <div className='h-[700px] bg-blue-500 w-full'></div>
                     <div className='h-[700px] bg-blue-500 w-full'></div>
